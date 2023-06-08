169fb0055bb71aca962c017ba4c137b362e4df98
Pavel Falaleev <pasf@yandex.ru>
2023-06-08 20:46:43 +0300

    dataclass

--- homework.py
+++ homework.py
@@ -4,12 +4,11 @@ from dataclasses import dataclass
 @dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    def __init__(self, training_type, duration, distance, speed, calories):
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
 
     def get_message(self) -> str:
         return (f'Тип тренировки: {self.training_type}; '
